07a8bca2dd62243519ca9f5b31127c2dc07159c6
Giacomo Zanotti <giacomozanotti@cuebiq.com>
2023-04-24 10:53:38 +0200

    stop method implemented.

--- news-poller/src/main/kotlin/com/oracolo/ilpost/poller/NewsExtractorPoller.kt
+++ news-poller/src/main/kotlin/com/oracolo/ilpost/poller/NewsExtractorPoller.kt
@@ -32,6 +32,6 @@ class NewsExtractorPoller : NewsPoller {
     }
 
     override fun stop() {
-        TODO("Not yet implemented")
+        scheduler.shutdown()
     }
 }
--- news-poller/src/test/kotlin/com/oracolo/ilpost/poller/NewsExtractorPollerTest.kt
+++ news-poller/src/test/kotlin/com/oracolo/ilpost/poller/NewsExtractorPollerTest.kt
@@ -7,11 +7,11 @@ import com.oracolo.ilpost.observer.NewsObserver
 import com.oracolo.ilpost.poller.profile.PollerProfile
 import io.quarkus.test.junit.QuarkusTest
 import io.quarkus.test.junit.TestProfile
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
-import org.mockito.Mockito.mock
-import org.mockito.Mockito.`when`
+import org.mockito.Mockito.*
 import java.util.concurrent.BlockingQueue
 import java.util.concurrent.LinkedBlockingQueue
 import java.util.concurrent.TimeUnit
@@ -29,6 +29,11 @@ class NewsExtractorPollerTest {
 
     private val linkedQueue: BlockingQueue<Set<News>> = LinkedBlockingQueue()
 
+    @AfterEach
+    fun reset() {
+        reset(dataExtractor)
+        linkedQueue.clear()
+    }
 
     @Test
     fun `should start polling`() {
@@ -56,6 +61,14 @@ class NewsExtractorPollerTest {
             })
         }
         Thread.sleep(3000)
+        Assertions.assertEquals(3, linkedQueue.size)
+    }
+
+    @Test
+    fun `should stop polling`() {
+        `should poll correctly every 1 seconds`()
+        newsPoller.stop()
+        Thread.sleep(1_000)
         Assertions.assertEquals(3, linkedQueue.size)
     }
 }
